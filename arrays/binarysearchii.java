597cb467e22f0feb5d4f8e66e318aeb2de786136
prashant260 <prashantagchoudhary2004@gmail.com>
2025-03-15 01:30:25 +0530

    binary search by check of sorted order

---
+++ arrays/binarysearchii.java
@@ -0,0 +1,85 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class binarysearchii {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        Scanner  in =new Scanner(System.in);
+    // creating an array
+        System.out.println("enter the size of the array: ");
+        int n=sc.nextInt();
+        int arr [] =new int[n];
+    // taking inputs in the array
+        System.out.println("enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();    
+        }System.out.println(Arrays.toString(arr));
+        
+        if (arr[0]<arr[arr.length-1]){
+            ascending(arr);}
+
+        if(arr[0]==arr[arr.length-1]){
+            System.out.println("the array consists of same element "+ arr[0]);
+            return;
+            
+        }
+
+        else{descending(arr);
+        }
+    }
+
+    static void ascending(int arr[]){
+        System.out.println("enter the element to search: ");
+        Scanner  in =new Scanner(System.in);
+        int target = in.nextInt();
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+        int mid = (start+end)/2;
+        
+        
+        if (arr[mid]==target){
+            System.out.println("at index: " + mid);
+
+        }
+        if(target>arr[mid]) {
+            start=mid+1;
+
+        }
+        else{
+            end = mid-1;
+
+        }
+        
+
+    }
+    }
+    static void descending(int arr[]){
+        System.out.println("enter the element to search: ");
+        Scanner  in =new Scanner(System.in);
+        int target = in.nextInt();
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+        int mid = (start+end)/2;
+        
+        
+        if (arr[mid]==target){
+            System.out.println("at index: " + mid);
+
+        }
+        if(target>arr[mid]) {
+            end = mid;
+
+        }
+        else{
+            start=mid+1;
+
+        }
+        
+
+    }
+    }
+}
